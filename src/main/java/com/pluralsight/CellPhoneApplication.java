648e4bfd1fccac949a30805d94ef9ec30b4b6fe7
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-01 10:37:50 -0400

    main comment

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,10 +1,14 @@
 package com.pluralsight;
 
 
+import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
 
+        CellPhone cellPhone1 = new CellPhone();
+
+        Scanner scanner = new Scanner(System.in);
     }
 
 }
